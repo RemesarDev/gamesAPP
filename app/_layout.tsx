8d3d472496a87435155abd583bb09e3bcb8802b6
RemesarDev <remesarjuanprogr@.com>
2025-10-23 18:44:59 -0300

    Fallas comentadas + Slot

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,7 @@ import {
 } from '@tanstack/react-query'
 import React from 'react'
 
+import { Slot } from 'expo-router'
 import '../global.css'
 
 const queryClient = new QueryClient()
@@ -11,7 +12,7 @@ const queryClient = new QueryClient()
 const RootLayout = () => {
   return (
     <QueryClientProvider client={queryClient}>
-    
+    <Slot/>
     </QueryClientProvider>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,22 +5,22 @@ module.exports = {
   presets: [require("nativewind/preset")],
   theme: {
     extend: {
-	colors: {
-	    ligth:{
-		primary: Colors.light.primary,
-		secondary: Colors.light.secondary,
-		tertiary: Colors.light.tertiary,
-		background: Colors.light.background,
-		text: Colors.light.text,
-	    },
-	    dark:{
-		primary: Colors.dark.primary,
-		secondary: Colors.dark.secondary,
-		tertiary: Colors.dark.tertiary,
-		background: Colors.dark.background,
-		text: Colors.dark.text,
-	    }
-	}   
+	//colors: {
+	//    ligth:{
+	//	primary: Colors.light.primary,
+	//	secondary: Colors.light.secondary,
+	//	tertiary: Colors.light.tertiary,
+	//	background: Colors.light.background,
+	//	text: Colors.light.text,
+	  //  },
+	 //   dark:{
+	//	primary: Colors.dark.primary,
+	//	secondary: Colors.dark.secondary,
+	//	tertiary: Colors.dark.tertiary,
+	//	background: Colors.dark.background,
+	//	text: Colors.dark.text,
+	  //  }
+	//}   
     },
   },
   plugins: [],
