62614cee25e60db51825c111db6cc379d446f955
RemesarDev <remesarjuanprogr@.com>
2025-10-23 18:30:24 -0300

    Instalacion de TanStack

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,18 @@
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 import React from 'react'
 
-import {Slot} from 'expo-router'
 import '../global.css'
+
+const queryClient = new QueryClient()
 
 const RootLayout = () => {
   return (
-    <Slot/>
+    <QueryClientProvider client={queryClient}>
+    
+    </QueryClientProvider>
   )
 }
 
