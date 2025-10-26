5da7279196a9820627d208451c4485fa1f026724
Josué Hernandez <128203138+HernandezDev@users.noreply.github.com>
2025-10-26 20:43:13 -0300

    Remove loading and error handling from FamilySection component

--- presentation/components/sections/FamilySection.tsx
+++ presentation/components/sections/FamilySection.tsx
@@ -1,7 +1,7 @@
 import GeneralHorizontalList from '@/presentation/components/generic/GeneralHorizontalList';
 import { usePlatformByFamily } from '@/presentation/hooks/usePlatformByFamily';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 
 interface Props {
     familyId: number;
@@ -10,21 +10,6 @@ interface Props {
 const FamilySection = ({ familyId }: Props) => {
     const { platformByFamilyQuery, familyQuery } = usePlatformByFamily(familyId);
 
-    if (platformByFamilyQuery.isLoading) {
-        return (
-            <View className="py-4 items-center">
-                <Text className="mt-2">Cargando {familyQuery.data?.name ?? ''}...</Text>
-            </View>
-        );
-    }
-
-    if (platformByFamilyQuery.isError) {
-        return (
-            <View className="py-4 items-center">
-                <Text>Error cargando {familyQuery.data?.name ?? ` ${familyId}`}</Text>
-            </View>
-        );
-    }
 
     const platforms = platformByFamilyQuery.data?.map((platform) => ({
         id: platform.id,
