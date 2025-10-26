5d39d408ba2888704b4408922f944bd2d717fd01
Josué Hernandez <128203138+HernandezDev@users.noreply.github.com>
2025-10-26 17:48:02 -0300

    ajustar texto de GeneralPoster

--- presentation/components/generic/GeneralPoster.tsx
+++ presentation/components/generic/GeneralPoster.tsx
@@ -2,29 +2,39 @@ import { Image, Text, View } from 'react-native';
 
 interface Props {
     id: number;
-    poster:string;
-    smallPoster?:boolean;
-    className?:string;
-    name?:string;
+    poster: string;
+    smallPoster?: boolean;
+    className?: string;
+    name?: string;
 }
 
-const GeneralPoster = ({poster,name,smallPoster=false,id}:Props) => {
+const GeneralPoster = ({ poster, name, smallPoster = false, id }: Props) => {
+    const imgWidth = smallPoster ? 85 : 150;
+    const imgHeight = smallPoster ? 130 : 250;
+
     return (
-       // <Pressable className={`active:opacity-90 px-2 ${className}`}
-        //onPress={()=>router.push(`/movie/${id}`)}> 
         <View className="items-center">
-        <Image
-            source={{ uri: poster }}
-            className="shadow-lg rounded-2xl"
-            style={{
-            width: smallPoster ? 85 : 150,
-            height: smallPoster ? 130 : 250,
-            }}
-            resizeMode="cover"
-        />
-        <Text className="text-center font-bold mb-2">{name}</Text>
+            <Image
+                source={{ uri: poster }}
+                className="shadow-lg rounded-2xl"
+                style={{
+                    width: imgWidth,
+                    height: imgHeight,
+                }}
+                resizeMode="cover"
+            />
+            <Text
+                style={{
+                    width: imgWidth,
+                    textAlign: 'center',
+                    fontWeight: '700',
+                    marginBottom: 8,
+                    flexWrap: 'wrap',
+                }}
+            >
+                {name}
+            </Text>
         </View>
-        //</Pressable>
-    )
-}
+    );
+};
 export default GeneralPoster
