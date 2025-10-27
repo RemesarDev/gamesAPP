156c1756a4cc719ce70139d3048e1281cb9e832e
julietalaurasalazar <julietalaurasalazar@gmail.com>
2025-10-27 17:02:41 -0300

    pagina home

---
+++ app/home/index.tsx
@@ -0,0 +1,54 @@
+import CustomButton from "@/presentation/components/generic/CustomButton";
+import MarcoFondo from "@/presentation/components/generic/MarcoFondo";
+import { useRouter } from "expo-router";
+import { Image, Text, View } from "react-native";
+
+const Home = () => {
+	
+	const router = useRouter();
+
+
+	return (
+		<MarcoFondo
+			colors={['black', 'black','black','#3A47D5', '#FF00C3']}>
+			<View className="flex-1 px-4 my-10">
+				<View className="mt-20">
+					<Text className="text-4xl font-extrabold text-center text-indigo-300 my-6">
+					gameAPP
+					</Text>
+					<Image
+						source={require('@/assets/images/invader-home.gif')}
+						style={{ width: '100%', height: '50%', alignSelf: 'center' }}
+						className='rounded-full'
+					/>
+				</View>
+
+				<View className="flex-1 mt-10 flex-row justify-center">
+					<CustomButton
+						icon="game-controller"
+						txtColor="white"
+						className={'border border-3 m-3 border-white w-[40%] h-[100px]'}
+						onPress={() =>
+							router.push('/')
+						} // Acción al presionar
+					>
+						Familias
+					</CustomButton>
+
+					<CustomButton
+						icon="person"
+						txtColor="white"
+						className={'border border-3 m-3 border-white w-[40%] h-[100px]'}
+						onPress={() =>
+							router.push('/profile/index')
+						} // Acción al presionar
+					>
+						Perfil
+					</CustomButton>
+				</View>											
+			</View>
+		</MarcoFondo>
+	);
+};
+
+export default Home;
