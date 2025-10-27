f43337106768fd801f70151b2ec28cdf5f0b1104
Josué Hernandez <128203138+HernandezDev@users.noreply.github.com>
2025-10-27 15:49:49 -0300

    Add FooterGame component and refactor GameScreen for improved structure

--- app/game/[id].tsx
+++ app/game/[id].tsx
@@ -1,27 +1,29 @@
 import MarcoFondo from '@/presentation/components/generic/MarcoFondo';
+import FooterGame from '@/presentation/components/sections/game/FooterGame';
 import { useGames } from '@/presentation/hooks/useGames';
 import { useLocalSearchParams } from 'expo-router';
 import React from 'react';
 import { ActivityIndicator, ScrollView, Text, View } from 'react-native';
 
 const GameScreen = () => {
-    const{id} = useLocalSearchParams();
-    const{gameQuery}=useGames(+id);
+  const { id } = useLocalSearchParams();
+  const { gameQuery } = useGames(+id);
 
-    if(gameQuery.isLoading || !gameQuery.data){
-      return(
-        <View className='flex flex-1 justify-center items-center'>
-          <Text className='mb-4'>Espere por favor</Text>
-          <ActivityIndicator color='purple' size={30}/>
-        </View>
-      )
-    }
+  if (gameQuery.isLoading || !gameQuery.data) {
+    return (
+      <View className='flex flex-1 justify-center items-center'>
+        <Text className='mb-4'>Espere por favor</Text>
+        <ActivityIndicator color='purple' size={30} />
+      </View>
+    )
+  }
 
   return (
 
     <MarcoFondo>
       <ScrollView>
-          <Text>{id}</Text>
+        <Text>{id}</Text>
+        <FooterGame id={id} />
       </ScrollView>
     </MarcoFondo>
   )
