44a2ec301469cd11b7e81cceaee2c8c5528abbf6
Josué Hernandez <128203138+HernandezDev@users.noreply.github.com>
2025-10-27 12:40:57 -0300

    Revert "Merge branch 'workBanchFariñas'"
    This reverts commit 7e079d607f7eeed854b78da71cac88d57604f903, reversing
    changes made to 69df0ef7004ca0b54d0a5353b3604c1a000ad52c.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,38 +1,20 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import React from "react";
-import { Drawer } from "expo-router/drawer";
-import { Ionicons } from "@expo/vector-icons"; // Importa Ionicons
-import "../global.css";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import React from 'react'
 
-const queryClient = new QueryClient();
+import { Slot } from 'expo-router'
+import '../global.css'
+
+const queryClient = new QueryClient()
 
 const RootLayout = () => {
-	return (
-		<QueryClientProvider client={queryClient}>
-			<Drawer>
-				<Drawer.Screen
-					name="index"
-					options={{
-						drawerLabel: "Inicio",
-						title: "GamesApp",
-						drawerIcon: ({ color, size }) => (
-							<Ionicons name="home-outline" size={size} color={color} /> // Icono de Inicio
-						),
-					}}
-				/>
-				<Drawer.Screen
-					name="family/[id]"
-					options={{
-						drawerLabel: "Franquicias",
-						title: "Franquicias",
-						drawerIcon: ({ color, size }) => (
-							<Ionicons name="albums-outline" size={size} color={color} /> // Icono de Franquicias
-						),
-					}}
-				/>
-			</Drawer>
-		</QueryClientProvider>
-	);
-};
+  return (
+    <QueryClientProvider client={queryClient}>
+    <Slot/>
+    </QueryClientProvider>
+  )
+}
 
-export default RootLayout;
+export default RootLayout
--- presentation/hooks/usePlatformsFamilies.tsx
+++ presentation/hooks/usePlatformsFamilies.tsx
@@ -1,11 +1,11 @@
-import { getPlatformFamiliesActions } from "@/core/actions/platform/getPlatformFamilies.actions";
-import { useQuery } from "@tanstack/react-query";
+import { getPlatformFamiliesActions } from '@/core/actions/platform/getPlatformFamilies.actions';
+import { useQuery } from '@tanstack/react-query';
 
 export const usePlatformsFamilies = () => {
-	const familyQuery = useQuery({
-		queryKey: ["family"],
-		queryFn: () => getPlatformFamiliesActions(),
-		staleTime: 1000 * 60 * 60 * 24,
-	});
-	return { familyQuery };
-};
+  const familyQuery = useQuery({
+    queryKey: ['family'],
+    queryFn: ()=>getPlatformFamiliesActions(),
+    staleTime: 1000 * 60 * 60 * 24,
+  })
+  return{familyQuery}
+}
